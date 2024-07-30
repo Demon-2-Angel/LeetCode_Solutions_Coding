513fb283e9911a00c5d09c0d57659f00c5856d60
Aniruddha Kumar <110346786+Demon-2-Angel@users.noreply.github.com>
2024-07-30 16:58:45 +0530

    Time: 58 ms (69.83%), Space: 17.7 MB (49.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        final_index = []
+        """final_index = []
         for i in range(len(nums)):
             for j in range(i):
                 if nums[i] + nums[j] == target:
                     final_index.append(i)
                     final_index.append(j)
         final_index.sort()
-        return final_index
-                    
+        return final_index"""
         
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
+        return []
