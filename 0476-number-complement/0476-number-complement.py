044d042be7156bc0cb4078476bf0bd3fbc678bc4
Aniruddha Kumar <110346786+Demon-2-Angel@users.noreply.github.com>
2024-08-22 16:29:12 +0530

    Time: 34 ms (61.42%), Space: 16.4 MB (58.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,5 +1,12 @@
+# class Solution:
+#     def findComplement(self, num: int) -> int:
+#         length = num.bit_length()
+#         mask = (1 << length) - 1
+#         return num ^ mask
+    
 class Solution:
     def findComplement(self, num: int) -> int:
-        length = num.bit_length()
-        mask = (1 << length) - 1
-        return num ^ mask
+        binary_num = bin(num)[2:]
+        
+        complement = ''.join('1' if bit=='0' else '0' for bit in binary_num)
+        return int(complement,2)
