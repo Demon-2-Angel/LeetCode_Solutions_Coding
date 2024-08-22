339b946da27b9c21dc7080b1cada907f399cd946
Aniruddha Kumar <110346786+Demon-2-Angel@users.noreply.github.com>
2024-08-22 17:07:42 +0530

    Time: 46 ms (62.49%), Space: 16.5 MB (53.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,24 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        v=[(1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I')]
+
+        r=""
+        while num:
+            for val,sym in v:
+                if num>=val:
+                    r+=sym
+                    num-=val
+                    break
+        return r
